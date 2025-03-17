e67e26b29c93a8b4c07e43c6bd7f3d254b843304
isai-mykyta <nktisai@gmail.com>
2025-03-17 18:25:06 +0200

    removed prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,5 @@
 import eslintPlugin from "@typescript-eslint/eslint-plugin";
 import eslintParser from "@typescript-eslint/parser";
-import prettierPlugin from "eslint-plugin-prettier";
-import prettierConfig from "eslint-config-prettier";
 
 export default [
   {
@@ -15,12 +13,10 @@ export default [
     },
     plugins: {
       "@typescript-eslint": eslintPlugin,
-      prettier: prettierPlugin,
     },
     rules: {
       ...eslintPlugin.configs.recommended.rules,
       ...eslintPlugin.configs["recommended-requiring-type-checking"].rules,
-      ...prettierConfig.rules,
       "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
       "@typescript-eslint/explicit-module-boundary-types": "off",
       "@typescript-eslint/no-explicit-any": "warn",
